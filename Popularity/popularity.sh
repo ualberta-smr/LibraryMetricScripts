5570762464e601891168b043fd808af78a9d6c94
relhajj <46651402+relhajj@users.noreply.github.com>
2019-05-30 16:29:31 -0600

    Update popularity.sh

--- Popularity/popularity.sh
+++ Popularity/popularity.sh
@@ -3,6 +3,7 @@
 #Description: This metric counts the number of client projects of the libraries in library.txt by searching for import statements through codes on GitHub
 #How to use: ./popularity 
 #Input: library.txt (file with library packages)
-#Output: resultsDATE1-DATE2.txt (file which will have the results of the popularity for each of the libraries)
+#Output: A test file called Results-start_Date-end_Date.txt (file which will have the results of the popularity for each of the libraries) 
+# The name of the results text file is dependant on the date that the script is run 
 
 python3 GitHub_Library_Search.py
