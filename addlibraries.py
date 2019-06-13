d988d084e9528288667f45fcc77e4cd26fe5dd20
relhajj <46651402+relhajj@users.noreply.github.com>
2019-06-13 14:27:55 -0600

    now read data from json file

--- addlibraries.py
+++ addlibraries.py
@@ -1,7 +1,8 @@
-#This file adds libraries from the librarydata.txt file
+#This file adds libraries from the LibraryData.json file
 
 import os
 import sys
+import json
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
 os.environ['DJANGO_SETTINGS_MODULE'] = 'metricwebsite.settings'
@@ -11,16 +12,17 @@ django.setup()
 
 from librarycomparison.models import Domain,Library
 
-with open('librarydata.txt') as f:
-    lines = f.readlines()
-lines = [x.strip() for x in lines]
 
+lines = []
+with open('librarydata.json', 'r') as myfile:
+    lines = json.loads(myfile.read())    
+    
 for line in lines:
-    content = line.split(',')
-    library_name = content[0]
-    domain_name = content[1]
-    repository = content[2]
-    tag = content[3]
+    library_name = line['RepoName']
+    domain_name = line['Domain']
+    repository = line['FullRepName']
+    tag = line['StackTags']    
+    
     domain = Domain.objects.filter(name=domain_name)
     if not domain.exists():
         domain = Domain()
