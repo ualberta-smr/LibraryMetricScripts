67938643958b555b44076945036fde97c36225a5
relhajj <46651402+relhajj@users.noreply.github.com>
2019-06-14 21:34:50 -0600

    Re-adding addlibraries.py before utility tool

--- addlibraries.py
+++ addlibraries.py
@@ -1,8 +1,7 @@
-#This file adds libraries from the LibraryData.json file
+#This file adds libraries from the librarydata.txt file
 
 import os
 import sys
-import json
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
 os.environ['DJANGO_SETTINGS_MODULE'] = 'metricwebsite.settings'
@@ -12,17 +11,16 @@ django.setup()
 
 from librarycomparison.models import Domain,Library
 
+with open('librarydata.txt') as f:
+    lines = f.readlines()
+lines = [x.strip() for x in lines]
 
-lines = []
-with open('librarydata.json', 'r') as myfile:
-    lines = json.loads(myfile.read())    
-    
 for line in lines:
-    library_name = line['RepoName']
-    domain_name = line['Domain']
-    repository = line['FullRepName']
-    tag = line['StackTags']    
-    
+    content = line.split(',')
+    library_name = content[0]
+    domain_name = content[1]
+    repository = content[2]
+    tag = content[3]
     domain = Domain.objects.filter(name=domain_name)
     if not domain.exists():
         domain = Domain()
