3385aa4e18e972f96793a518268816243ef6271d
relhajj <46651402+relhajj@users.noreply.github.com>
2019-06-17 14:49:03 -0600

    Updated last modified date to use utility tool

--- LastModificationDate/lastmodificationdate.py
+++ LastModificationDate/lastmodificationdate.py
@@ -4,7 +4,7 @@
 # - You will need to install PyGithub
 # - You will need to input your Github credentials to make use of the Github API
 #Input:
-# - A file with the library repository names (SharedFiles/repositories.txt)
+# - A file with the library repository names (LibraryData.json)
 #Output:
 # A pickle file called lastmodificationdate.pkl, which will contain a dictionary where the key is a library repository, and the value is each key is
 #a string containing dates in format %m/%d/%Y separated by semicolons:
@@ -17,6 +17,7 @@ import sys
 import pickle
 from github import Github, Repository, GitTag
 import getpass
+import json
 
 def loadLastModificationDateData():
 	data = {}
@@ -35,34 +36,30 @@ def saveData(data):
 		pickle.dump(data, output, pickle.HIGHEST_PROTOCOL)
 
 def getLastModificationDates(username, password):
-        data = loadLastModificationDateData()
-
-        rel_path = "SharedFiles/repositories.txt"
-        if os.path.isdir('SharedFiles'):
-                file_path = rel_path
-        else:
-                file_path = os.path.join(os.pardir, rel_path)
-        
-        with open(file_path) as f:
-                repositories = f.readlines()
-        repositories = [x.strip() for x in repositories]
-
-        g = Github(username, password)
-
-        for repository in repositories:
-                r = g.get_repo(repository)
-                dates_string = ""
-                i = 0
-                for c in r.get_commits():
-                        if i == 10:
-                                break
-                        if i > 0:
-                                dates_string += ';'
-                        dates_string += c.commit.author.date.strftime('%m/%d/%Y')
-                        i += 1
-                data[repository] = dates_string
-                saveData(data)
-
+	
+	data = loadLastModificationDateData()
+	
+	repositories = []
+	with open('../LibraryData.json', 'r') as f:
+		LibraryData = json.loads(f.read()) 
+	for line in LibraryData:
+		repositories.append(line['FullRepoName'])
+	
+	g = Github(username, password)
+	
+	for repository in repositories:
+		r = g.get_repo(repository)
+		dates_string = ""
+		i = 0
+		for c in r.get_commits():
+			if i == 10:
+				break
+			if i > 0:
+				dates_string += ';'
+			dates_string += c.commit.author.date.strftime('%m/%d/%Y')
+			i += 1
+		data[repository] = dates_string
+		saveData(data)
 
 def main():
         if len(sys.argv) == 3:
