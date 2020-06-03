354c888dda00347e333157118d5a521944bfd889
Sarah Nadi <nadi@ualberta.ca>
2020-06-03 17:42:29 -0600

    Optimizing issue retrieval: since last date in DB, directly save to DB

--- scripts/IssueMetrics/issues.py
+++ scripts/IssueMetrics/issues.py
@@ -33,10 +33,19 @@ from securityclassifier import SecurityClassifier
 import json
 from CommonUtilities import Common_Utilities
 
+
 #This makes the utility_tool visible from this file
 import sys
 sys.path.append('../')
 from SharedFiles.utility_tool import read_json_file
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'librarycomparison.settings'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "librarycomparison.settings")
+import django
+django.setup()
+from librarycomparison.models import Library, Issue
 
 issue_closing_time_pkl = 'IssueMetrics/issueclosingtime.pkl'
 issue_data_pkl = 'IssueMetrics/issuedata.pkl'
@@ -78,6 +87,32 @@ class IssueData:
   def __str__(self):
     return self.issue_id
 
+def saveIssueInDB(issue, ):
+    data = loadData('issuedata.pkl')
+    for repo, issues in data.items():
+        total_issues = 0
+        total_performance_issues = 0
+        total_security_issues = 0
+        library = Library.objects.get(repository=repo)
+        metricsentry = MetricsEntry.objects.filter(library=library).latest('created_on')
+        for i in issues:
+            total_issues += 1
+            issue = Issue()
+            issue.issue_id = str(i.issue_id)
+            issue.creation_date = i.creation_date
+            issue.closing_date = i.closing_date
+            issue.first_response_date = i.first_comment_date
+            issue.performance_issue = i.performance_issue
+            issue.security_issue = i.security_issue
+            if issue.performance_issue == True:
+                total_performance_issues += 1
+            if issue.security_issue == True:
+                total_security_issues += 1
+            issue.library = library
+            issue.save()
+            library.issue_set.add(issue)
+            library.save()
+
 
 def loadData(filename):
   data = {}
@@ -109,20 +144,23 @@ def calculateAverageClosingTime():
     issue_closing_times[repo] = float(total_closing_time/total_closed_issues/86400)
   saveData(issue_closing_times, issue_closing_time_pkl)
 
+##TODO: calculate this during filling db as aggregation
 def calculateAverageResponseTime():
-  issue_data = loadData(issue_data_pkl)
+  # issue_data = loadData(issue_data_pkl)
   issue_response_times = {}
-  for repo, issues in issue_data.items():
+  for library in Library.objects.all():
     total_response_time = 0
     total_issues_with_comments = 0
-    for issue in issues:
-      if issue.first_comment_date == None:
+    for issue in Issue.objects.get(library=library):
+      if issue.first_response_date == None:
         continue
-      response_time = int((issue.first_comment_date - issue.creation_date).total_seconds())
+      response_time = int((issue.first_response_date - issue.creation_date).total_seconds())
       total_response_time += response_time
       total_issues_with_comments += 1
-    issue_response_times[repo] = float(total_response_time/total_issues_with_comments/86400)
-  saveData(issue_response_times, issue_resp_time_pkl)
+      #issue_response_times[repo] = float(total_response_time/total_issues_with_comments/86400)
+      metricsentry = MetricsEntry.objects.filter(library=library)
+      metricsentry.repsonse_time = float(total_response_time/total_issues_with_comments/86400)
+  #saveData(issue_response_times, issue_resp_time_pkl)
 
 def getIssueData(token, arr):
 
@@ -131,38 +169,47 @@ def getIssueData(token, arr):
     if line['JIRAURL'] == "":
       repositories.append(line['FullRepoName'])
  
-  issue_data = loadData(issue_data_pkl)
+  #issue_data = loadData(issue_data_pkl)
 
-  if issue_data == None:
-    issue_data = {}
+  #if issue_data == None:
+  #  issue_data = {}
 
-  g = Github(token)
+  github = Github(token)
 
-  for repository in repositories:
+  for repo_name in repositories:
     first_issue = 1
-    print("Current repository: ", repository)
+    print("Current repository: ", repo_name)
 
     try:
-      r = g.get_repo(repository)
+      repo = github.get_repo(repo_name)
 
-      max_issue_number = r.get_issues(state="all")[0].number
+      #get latest data we have about this repo
+      library = Library.objects.get(github_repo=repo_name)
+      print("got library", library.name)
+      latest_issue_date = datetime(1700,1,1)
+      issues = Issue.objects.filter(library=library)
+
+      if issues:
+        latest_issue_date = issues.latest('creation_date').creation_date
+
+      max_issue_number = repo.get_issues(state="all", since=latest_issue_date)[0].number
+
     except RateLimitExceededException:
-      print("Rate Limit Exceeded... Sleeping 1 hour")
-      time.sleep(60*60)
-      g = Github(token)
-      r = g.get_repo(repository)
-      max_issue_number = r.get_issues(state="all")[0].number
+      Common_Utilities.go_to_sleep("API limit exceeded,Go to sleep for ", 60)
+      repo = github.get_repo(repo_name)
+      max_issue_number = r.get_issues(state="all",since=latest_issue_date)[0].number
+
+    print("we have ", max_issue_number, "that we will loop through now")
 
     for i in range(first_issue, max_issue_number):
+      print("processing issue ", i)
       try:
-        issue = r.get_issue(i)
+        gh_issue = repo.get_issue(i)
       except UnknownObjectException:
         continue
       except RateLimitExceededException:
-        print("Rate Limit Exceeded... Sleeping 1 hour") 
-        time.sleep(60*60)
-        g = Github(token)
-        r = g.get_repo(repository)
+        Common_Utilities.go_to_sleep("API limit exceeded,Go to sleep for ", 60)
+        #repo = g.get_repo(repo_name)
         i -= 1
         continue
       except:
@@ -172,34 +219,33 @@ def getIssueData(token, arr):
       if issue.pull_request != None:
         continue
 
-      new_issue = IssueData(i)
-      new_issue.addTitle(issue.title)
-      new_issue.addCreationDate(issue.created_at)
-      new_issue.addClosingDate(issue.closed_at)
+      new_issue = Issue()
+      new_issue.issue_id = str(gh_issue.issue_id)
+      new_issue.creation_date = gh_issue.created_at
+      new_issue.title = gh_issue.title
+      new_issue.closing_date = gh_issue.closed_at
+      new_issue.library = library
 
       while True:
         try:
           for comment in issue.get_comments():
             if comment.user == issue.user:
               continue
-            new_issue.addFirstResponseDate(comment.created_at)
+            new_issue.first_response_date(comment.created_at)
             break
           break
         except RateLimitExceededException:
-          print("Rate Limit Exceeded... Sleeping 1 hour")
-          time.sleep(60*60)
-          g = Github(token)
-          r = g.get_repo(repository)
-          issue = r.get_issue(i)
+          Common_Utilities.go_to_sleep("API limit exceeded,Go to sleep for ", quick_sleep)
+          #g = Github(token)
+          #r = g.get_repo(repository)
+          #issue = r.get_issue(i)
         except Exception as e:
           print(e)
           continue
         
-      if repository in issue_data:
-        issue_data[repository].append(new_issue)
-      else:
-        issue_data[repository] = [new_issue]
-      saveData(issue_data, issue_data_pkl)
+      issue.save()
+      #not sure if we need this: library.issue_set.add(issue) library.save()
+    Common_Utilities.go_to_sleep("Sleeping before next library..Go to sleep for ", 180)
 
 def getIssueDataJIRA(urls):
   dict = {}
@@ -273,14 +319,15 @@ def main():
 
   print("Getting issue data")  
   getIssueDataJIRA(lib_data_json)
+  print("Got jira issue data")
   getIssueData(config_dict["TOKEN"], lib_data_json)
 
-  print("Calculating avg response time")  
-  calculateAverageResponseTime()
-  print("Calculating avg closing time")  
-  calculateAverageClosingTime()
-  print("Applying classifiers")
-  applyClassifiers()
+  # print("Calculating avg response time")  
+  # calculateAverageResponseTime()
+  # print("Calculating avg closing time")  
+  # calculateAverageClosingTime()
+  # print("Applying classifiers")
+  # applyClassifiers()
 
 if __name__ == "__main__":
   main()
