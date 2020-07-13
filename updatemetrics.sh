3f230d33791088573a3e49a72db1186c14ca80c0
Sarah Nadi <nadi@ualberta.ca>
2020-07-13 11:37:08 -0600

    Add script for monthly update

---
+++ updatemetrics.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+now="$(date +'%d/%m/%Y')"
+echo "Starting to update metrics at $now..."
+
+#backup current database first 
+
+echo "Backing up database first ... "
+
+today=$(date +'%m-%d-%Y')
+
+mysqldump -u libcomp --no-create-info --complete-insert --skip-triggers libcomp  Domain Library Issue Metric LibraryRelease MetricsEntry Chart ProjectType TeamType PluginUser PluginUser_groups PluginUser_projects PluginUser_teams PluginFeedback WebsiteFeedback > "/home/webfiles/DBBackups/libcomp-bkp-${today}.sql"
+
+#invoke script
+mkdir -p logs
+
+nohup python3 -u scripts > logs/libcomp-${today}.log &
+
+echo "Finished running scripts.. now copying charts"
+
+cp scripts/charts/*.pkl /var/www/librarycomparisonswebsite/charts/
+
+
+echo "Finished whole process at $now..."
