552a8e4bdfce06556e02a6f5ae55f5da00879d67
Sarah Nadi <nadi@ualberta.ca>
2020-07-15 15:26:13 -0600

    Add update metrics script

--- updatemetrics.sh
+++ updatemetrics.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
+# set -e
+
 now="$(date +'%d/%m/%Y')"
 echo "Starting to update metrics at $now..."
 
-#backup current database first (note that this relies on your DB password being stored in ~/.my.cnf)
+#backup current database first (note that this relies on your DB username & password being stored in ~/.my.cnf)
 
 echo "Backing up database first ... "
 
-today=$(date +'%m-%d-%Y')
+mkdir -p ~/DBBackups
 
-mysqldump -u libcomp --no-create-info --complete-insert --skip-triggers libcomp  Domain Library Issue Metric LibraryRelease MetricsEntry Chart ProjectType TeamType PluginUser PluginUser_groups PluginUser_projects PluginUser_teams PluginFeedback WebsiteFeedback > "/home/webfiles/DBBackups/libcomp-bkp-${today}.sql"
+today=$(date +'%m-%d-%Y')
+file_name="~/DBBackups/libcomp-bkp-${today}.sql"
+mysqldump --no-create-info --complete-insert --skip-triggers libcomp  Domain Library Issue Metric LibraryRelease MetricsEntry Chart ProjectType TeamType PluginUser PluginUser_groups PluginUser_projects PluginUser_teams PluginFeedback WebsiteFeedback > file_name
 
 #invoke script
 mkdir -p logs
 
-nohup python3 -u scripts > logs/libcomp-${today}.log &
+echo "Invoking scripts..."
 
-echo "Finished running scripts.. now copying charts"
+python3 -u -m scripts > logs/libcomp-${today}.log
+
+echo "Finished running scripts.. now copying charts to website"
 
 cp scripts/charts/*.pkl /var/www/librarycomparisonswebsite/charts/
 
